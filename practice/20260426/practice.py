2d259d6f059288cce82608f9ee5e0e9534112f12
Your Name <you@example.com>
2026-04-26 01:05:01 +0900

    20260426_atccoder開始

---
+++ practice/20260426/practice.py
@@ -0,0 +1,13 @@
+# atcoder practice
+
+# NとMを読み込む
+N, M = map(int, input().split())
+
+# 服のリスト F を読み込む
+F = list(map(int, input().split()))
+
+# 質問 1: N 人全員が異なる種類の服を着ていますか？
+diff = (1/M)^(N - 1)
+
+print(diff)
+print("\n")
