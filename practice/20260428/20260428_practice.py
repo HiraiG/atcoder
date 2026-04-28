eaf8ea5863591c1fab7f8ad82ea32db1894b6110
Your Name <you@example.com>
2026-04-28 23:51:20 +0900

    20260428_コイン問題の開始

--- practice/20260428/20260428_practice.py
+++ practice/20260428/20260428_practice.py
@@ -5,6 +5,21 @@ A = list(map(int, input().split()))
 
 count = 0
 
+while True:
+    flag = False
+    for i in range(N):
+        if A[i] % 2 != 0:
+            flag = True
+            break
+    
+    if flag:
+        break
 
+    for i in range(N):
+        A[i] = A[i]//2
+
+    count += 1
+
+print(count)    
 
 
