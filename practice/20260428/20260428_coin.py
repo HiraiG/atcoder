3e53fce7a6c119111f995d0e9ccbe808f0937bd9
Your Name <you@example.com>
2026-05-02 21:40:20 +0900

    20260502_業務開始

--- practice/20260428/20260428_coin.py
+++ practice/20260428/20260428_coin.py
@@ -5,6 +5,7 @@ count = 0
 for i in range(A+1):
     for j in range(B+1):
         for k in range(C+1): 
-            500*i + 100*j + 50*k == X
+           if 500*i + 100*j + 50*k == X:
+                count +=1 
 
-print(A, B, C, X)
+print(count)
